e119090e03930e06d6881cc5f41a1147e94b98f5
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2021-12-12 21:28:51 -0500

    fixing logger config

--- server/config/logger.js
+++ server/config/logger.js
@@ -1,4 +1,7 @@
 const winston = require('winston');
+const morgan = require('morgan');
+const stripFinalNewline = require('strip-final-newline');
+const { v4: uuidv4 } = require('uuid');
 
 const logger = winston.createLogger({
   format: winston.format.simple(),
@@ -7,5 +10,19 @@ const logger = winston.createLogger({
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
   ],
 });
+const requestId = (req, res, next) => {
+  const { headers } = req;
+  const id = headers['X-Request-Id'] ?? uuidv4();
+  req.id = id;
+  next();
+};
+morgan.token('id', (req) => req.id);
 
-module.exports = logger;
+const requestLog = morgan(
+  ':remote-addr [:date[iso]] :id ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
+  {
+    stream: { write: (message) => logger.info(stripFinalNewline(message)) },
+  },
+);
+
+module.exports = { logger, requestId, requestLog };
--- server/index.js
+++ server/index.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const morgan = require('morgan');
-const { v4: uuidv4 } = require('uuid');
 
 const api = require('./api/v1');
-const logger = require('./config/logger');
+const { logger, requestId, requestLog } = require('./config/logger');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader('X-Request-Id', uuidv4());
-});
-app.use(
-  morgan('combined', { stream: { write: (message) => logger.info(message) } }),
-);
+app.use(requestId);
+app.use(requestLog);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,10 +16,12 @@ app.use('/api/1', api);
 app.use((req, res, next) => {
   const statusCode = 400;
   const message = 'Error. Route not found';
+  logger.warn(message);
   next({ statusCode, message });
 });
 app.use((err, req, res, next) => {
   const { statusCode = 500, message = '' } = err;
+  logger.error(message);
   res.status(statusCode).json({ message });
 });
 
