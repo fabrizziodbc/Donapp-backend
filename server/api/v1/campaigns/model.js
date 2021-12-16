9856041f674ceea5875666bbf6304c4c9b268a0e
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2021-12-16 07:57:22 -0500

    add mongoose Schemas, and validator(express validator)

---
+++ server/api/v1/campaigns/model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const campaign = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  img: { type: String, required: true },
+  tags: {
+    type: String,
+    enum: ['In memorium', 'Salud', 'Mascotas', 'Otros'],
+    required: true,
+  },
+  donations: { type: Number, default: 0 },
+  goal: { type: Number, min: [50, 'must set 50 USD minimum'], required: true },
+  name: { type: String, required: true },
+  campaignReason: { type: String, required: true },
+  commentsDb: { type: Array, default: [] },
+  date: { type: Date, default: Date.now },
+  active: { type: Boolean, default: true },
+});
+
+const model = mongoose.model('campaign', campaign);
+module.exports = model;
