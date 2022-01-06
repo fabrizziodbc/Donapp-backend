d49b13ade54db10520ac1baf9bf32bb0e97e93c3
torvic <130111@unsaac.edu.pe>
2022-01-06 17:15:24 -0500

    Add email and password validation

--- server/api/v1/user/routes.js
+++ server/api/v1/user/routes.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const { body } = require('express-validator');
 const userController = require('./controller');
 
 const router = express.Router();
 
-router.post('/signup', userController.signUp);
+router.post(
+  '/signup',
+  body('email', 'Email is required').notEmpty(),
+  body('password', 'Password is required').notEmpty(),
+  userController.signUp,
+);
+router.post(
+  '/signin',
+  body('email', 'Email is required').notEmpty(),
+  body('password', 'Password is required').notEmpty(),
+  userController.signIn,
+);
 
 module.exports = router;
