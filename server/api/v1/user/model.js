712633adaa95b1b21bdb2c4c135df58332b7a77a
torvic <130111@unsaac.edu.pe>
2022-01-06 17:13:46 -0500

    Apply password hash  & validating user's password

--- server/api/v1/user/model.js
+++ server/api/v1/user/model.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema(({
@@ -6,6 +7,25 @@ const userSchema = mongoose.Schema(({
   email: { type: String, required: true },
   password: { type: String, required: true },
 }));
+
+/**
+ * Password hash middleware
+ */
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (!user.isModified('password')) { return next(); }
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(user.password, salt);
+  user.password = hash;
+  return next();
+});
+
+/**
+ * Helper method for validating user's password
+ */
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
