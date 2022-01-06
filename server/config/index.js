c11ad8b0a3274cb19a643c8f801bd6c69503731c
torvic <130111@unsaac.edu.pe>
2022-01-06 17:16:08 -0500

    Add jwtsecret to config

--- server/config/index.js
+++ server/config/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const config = {
   port: process.env.PORT || 3000,
+  jwtsecret: 'Your secret is here',
   database: {
     url: process.env.DATABASE_URL,
     username: process.env.DATABASE_USERNAME,
