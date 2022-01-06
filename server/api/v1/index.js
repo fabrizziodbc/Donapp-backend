099db47961b0545ec0d45c7b58e01386f6be61e4
torvic <130111@unsaac.edu.pe>
2022-01-06 15:47:59 -0500

    Add the models, controllers & routes user

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const campaigns = require('./campaigns/routes');
+const userRouter = require('./user/routes');
 
 const router = express.Router();
 router.use('/campaigns', campaigns);
+router.use('/user', userRouter);
 
 module.exports = router;
