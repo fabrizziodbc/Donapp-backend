9834401b0911a1ae79ea3ee1ea780d594dd0842b
torvic <130111@unsaac.edu.pe>
2022-01-10 19:57:17 -0500

    Add getEnv function & use it in config

--- server/config/index.js
+++ server/config/index.js
@@ -1,10 +1,21 @@
 require('dotenv').config();
 
+const getEnv = () => {
+  let databaseUrl;
+  if (process.env.NODE_ENV === 'development') {
+    databaseUrl = process.env.DATABASE_URL_DEV;
+  }
+  if (process.env.NODE_ENV === 'test') {
+    databaseUrl = process.env.DATABASE_URL_TEST;
+  }
+  return databaseUrl;
+};
+
 const config = {
   port: process.env.PORT || 5000,
   jwtsecret: 'Your secret is here',
   database: {
-    url: process.env.DATABASE_URL,
+    url: getEnv(),
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
   },
