7010b853939859149989240dbd72bf26b46aee4c
torvic <130111@unsaac.edu.pe>
2022-01-18 19:43:54 -0500

    Create & Delete campaign with mongoose transaction

--- server/config/index.js
+++ server/config/index.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 const getEnv = () => {
   let databaseUrl;
   if (process.env.NODE_ENV === 'development') {
-    databaseUrl = process.env.DATABASE_URL_DEV;
+    databaseUrl = process.env.DATABASE_DEV;
   }
   if (process.env.NODE_ENV === 'test') {
-    databaseUrl = process.env.DATABASE_URL_TEST;
+    databaseUrl = process.env.DATABASE_TEST;
   }
   return databaseUrl;
 };
@@ -15,9 +15,10 @@ const config = {
   port: process.env.PORT || 5000,
   jwtsecret: 'Your secret is here',
   database: {
-    url: getEnv(),
+    name: getEnv(),
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
+    url: process.env.DATABASE_URL,
   },
 };
 
