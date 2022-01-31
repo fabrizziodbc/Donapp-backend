3172583b1d1d359b3e2b2409a383783aa8dec0cd
torvic <130111@unsaac.edu.pe>
2022-01-31 12:25:24 -0500

    Add updateUser & addBillingCustomerId services to user

---
+++ server/api/v1/user/service.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-underscore-dangle */
+const { get } = require('lodash');
+const User = require('./model');
+
+async function updateUser(id, user) {
+  const newUser = await User.findByIdAndUpdate(id, user, { new: true });
+  return newUser;
+}
+
+async function addBillingCustomerId(user, customerId) {
+  const creditCards = get(user, 'billing.creditCards', []);
+
+  const customer = {
+    billing: {
+      creditCards,
+      customerId,
+    },
+  };
+  const newUser = await User.findByIdAndUpdate(user._id, customer, { new: true });
+  return newUser;
+}
+
+module.exports = {
+  updateUser,
+  addBillingCustomerId,
+};
