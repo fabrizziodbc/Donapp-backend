a697d799dc8c6a88cb10b3ffdb1c9c112a432340
torvic <130111@unsaac.edu.pe>
2022-01-31 12:24:43 -0500

    Add creaditCardSchema & billingSchema to user model

--- server/api/v1/user/model.js
+++ server/api/v1/user/model.js
@@ -1,24 +1,62 @@
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema(({
+const creditCardSchema = new mongoose.Schema({
+  expMonth: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  expYear: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  mask: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  tokenId: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+}, { _id: false });
+
+const billingSchema = new mongoose.Schema({
+  creditCards: [creditCardSchema],
+  customerId: String,
+}, { _id: false });
+
+const userSchema = mongoose.Schema({
   name: { type: String, required: true },
   surname: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  campaigns: [{
-    type: mongoose.Types.ObjectId,
-    require: true,
-    ref: 'campaign',
-  }],
-}));
+  billing: billingSchema,
+  campaigns: [
+    {
+      type: mongoose.Types.ObjectId,
+      require: true,
+      ref: 'campaign',
+    },
+  ],
+}, { timestamps: true });
 
 /**
  * Password hash middleware
  */
 userSchema.pre('save', async function (next) {
   const user = this;
-  if (!user.isModified('password')) { return next(); }
+  if (!user.isModified('password')) {
+    return next();
+  }
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(user.password, salt);
   user.password = hash;
