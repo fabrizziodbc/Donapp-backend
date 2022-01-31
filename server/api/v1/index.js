4c62f1910333bb35fc7fdd04c9822f9e19c57f00
torvic <130111@unsaac.edu.pe>
2022-01-31 18:55:29 -0500

    Add payment routers

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const campaigns = require('./campaigns/routes');
 const userRouter = require('./user/routes');
 const filesRouter = require('./files/routes');
+const paymentRouter = require('./payment/routes');
 
 const router = express.Router();
 router.use('/campaigns', campaigns);
 router.use('/user', userRouter);
 router.use('/files', filesRouter);
+router.use('/payment', paymentRouter);
 
 module.exports = router;
