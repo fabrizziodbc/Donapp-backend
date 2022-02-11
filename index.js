69fa3794a4472947c8a1eeccad4b43dd84f47777
torvic <130111@unsaac.edu.pe>
2022-02-10 20:34:57 -0500

    Install socket io & use it in the project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const app = require('./server');
 const { connect } = require('./server/database');
 const config = require('./server/config');
+const { connect: connectSocket, socket } = require('./server/config/socket');
 
 const { database, port } = config;
 
@@ -16,7 +17,16 @@ connect({
 
 const server = http.createServer(app);
 
+connectSocket(server);
 const listen = server.listen(port, () => {
+  // socket status
+  socket.io.on('connection', (resSocket) => {
+    console.log(`Socket connected: ${resSocket.id}`);
+  });
+  socket.io.on('disconnect', (resSocket) => {
+    console.log(`Socket disconnected: ${resSocket.id}`);
+  });
+
   console.log(`server running at ${port}`);
 });
 
