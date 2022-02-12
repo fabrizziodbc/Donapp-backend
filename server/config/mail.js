9dfecff189f6b0e3de8a85a0c62a473c55b3f427
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2022-02-11 21:28:08 -0500

    add donation confirm email

--- server/config/mail.js
+++ server/config/mail.js
@@ -23,12 +23,12 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-const sendEmail = async (email, html) => {
+const sendEmail = async (email, subject, html) => {
   try {
     await transporter.sendMail({
       from: ` <${mail.user}>`,
       to: email,
-      subject: "Confirmación de cuenta DonApp",
+      subject,
       html,
     });
   } catch (error) {
@@ -297,8 +297,247 @@ const getTemplate = (name, token) => {
     </html>
       `;
 };
+const donationSucTemplate = (name, benefited, ammout) => {
+  return `<head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+        <!--[if !mso]><!-->
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <!--<![endif]-->
+        <!--[if (gte mso 9)|(IE)]>
+        <xml>
+          <o:OfficeDocumentSettings>
+            <o:AllowPNG/>
+            <o:PixelsPerInch>96</o:PixelsPerInch>
+          </o:OfficeDocumentSettings>
+        </xml>
+        <![endif]-->
+        <!--[if (gte mso 9)|(IE)]>
+    <style type="text/css">
+      body {width: 600px;margin: 0 auto;}
+      table {border-collapse: collapse;}
+      table, td {mso-table-lspace: 0pt;mso-table-rspace: 0pt;}
+      img {-ms-interpolation-mode: bicubic;}
+    </style>
+  <![endif]-->
+        <style type="text/css">
+      body, p, div {
+        font-family: inherit;
+        font-size: 14px;
+      }
+      body {
+        color: #000000;
+      }
+      body a {
+        color: #000000;
+        text-decoration: none;
+      }
+      p { margin: 0; padding: 0; }
+      table.wrapper {
+        width:100% !important;
+        table-layout: fixed;
+        -webkit-font-smoothing: antialiased;
+        -webkit-text-size-adjust: 100%;
+        -moz-text-size-adjust: 100%;
+        -ms-text-size-adjust: 100%;
+      }
+      img.max-width {
+        max-width: 100% !important;
+      }
+      .column.of-2 {
+        width: 50%;
+      }
+      .column.of-3 {
+        width: 33.333%;
+      }
+      .column.of-4 {
+        width: 25%;
+      }
+      ul ul ul ul  {
+        list-style-type: disc !important;
+      }
+      ol ol {
+        list-style-type: lower-roman !important;
+      }
+      ol ol ol {
+        list-style-type: lower-latin !important;
+      }
+      ol ol ol ol {
+        list-style-type: decimal !important;
+      }
+      @media screen and (max-width:480px) {
+        .preheader .rightColumnContent,
+        .footer .rightColumnContent {
+          text-align: left !important;
+        }
+        .preheader .rightColumnContent div,
+        .preheader .rightColumnContent span,
+        .footer .rightColumnContent div,
+        .footer .rightColumnContent span {
+          text-align: left !important;
+        }
+        .preheader .rightColumnContent,
+        .preheader .leftColumnContent {
+          font-size: 80% !important;
+          padding: 5px 0;
+        }
+        table.wrapper-mobile {
+          width: 100% !important;
+          table-layout: fixed;
+        }
+        img.max-width {
+          height: auto !important;
+          max-width: 100% !important;
+        }
+        a.bulletproof-button {
+          display: block !important;
+          width: auto !important;
+          font-size: 80%;
+          padding-left: 0 !important;
+          padding-right: 0 !important;
+        }
+        .columns {
+          width: 100% !important;
+        }
+        .column {
+          display: block !important;
+          width: 100% !important;
+          padding-left: 0 !important;
+          padding-right: 0 !important;
+          margin-left: 0 !important;
+          margin-right: 0 !important;
+        }
+        .social-icon-column {
+          display: inline-block !important;
+        }
+      }
+    </style>
+        <!--user entered Head Start--><link href="https://fonts.googleapis.com/css?family=Viga&display=swap" rel="stylesheet"><style>
+      body {font-family: 'Viga', sans-serif;}
+  </style><!--End Head user entered-->
+      </head>
+      <body>
+        <center class="wrapper" data-link-color="#000000" data-body-style="font-size:14px; font-family:inherit; color:#000000; background-color:#FFFFFF;">
+          <div class="webkit">
+            <table cellpadding="0" cellspacing="0" border="0" width="100%" class="wrapper" bgcolor="#FFFFFF">
+              <tr>
+                <td valign="top" bgcolor="#FFFFFF" width="100%">
+                  <table width="100%" role="content-container" class="outer" align="center" cellpadding="0" cellspacing="0" border="0">
+                    <tr>
+                      <td width="100%">
+                        <table width="100%" cellpadding="0" cellspacing="0" border="0">
+                          <tr>
+                            <td>
+                              <!--[if mso]>
+      <center>
+      <table><tr><td width="600">
+    <![endif]-->
+                                      <table width="100%" cellpadding="0" cellspacing="0" border="0" style="width:100%; max-width:600px;" align="center">
+                                        <tr>
+                                          <td role="modules-container" style="padding:0px 0px 0px 0px; color:#000000; text-align:left;" bgcolor="#FFFFFF" width="100%" align="left"><table class="module preheader preheader-hide" role="module" data-type="preheader" border="0" cellpadding="0" cellspacing="0" width="100%" style="display: none !important; mso-hide: all; visibility: hidden; opacity: 0; color: transparent; height: 0; width: 0;">
+      <tr>
+        <td role="module-content">
+          <p></p>
+        </td>
+      </tr>
+    </table><table border="0" cellpadding="0" cellspacing="0" align="center" width="100%" role="module" data-type="columns" style="padding:0px 0px 0px 0px;" bgcolor="#dde6de" data-distribution="1">
+      <tbody>
+        <tr role="module-content">
+          <td height="100%" valign="top"><table width="580" style="width:580px; border-spacing:0; border-collapse:collapse; margin:0px 10px 0px 10px;" cellpadding="0" cellspacing="0" align="left" border="0" bgcolor="" class="column column-0">
+        <tbody>
+          <tr>
+            <td style="padding:0px;margin:0px;border-spacing:0;"><table class="module" role="module" data-type="spacer" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="10cc50ce-3fd3-4f37-899b-a52a7ad0ccce">
+      <tbody>
+        <tr>
+          <td style="padding:0px 0px 40px 0px;" role="module-content" bgcolor="">
+          </td>
+        </tr>
+      </tbody>
+    </table></td>
+          </tr>
+        </tbody>
+      </table></td>
+        </tr>
+      </tbody>
+    </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="bff8ffa1-41a9-4aab-a2ea-52ac3767c6f4" data-mc-module-version="2019-10-22">
+      <tbody>
+        <tr>
+          <td style="padding:18px 30px 18px 30px; line-height:40px; text-align:inherit; background-color:#dde6de;" height="100%" valign="top" bgcolor="#dde6de" role="module-content"><div><div style="font-family: inherit; text-align: center"><span style="font-family: inherit; color: #444545; font-size: 48px"><strong>DonApp</strong></span><span style="font-family: inherit; color: #ff0056; font-size: 96px">.</span></div><div></div></div></td>
+        </tr>
+      </tbody>
+    </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="bff8ffa1-41a9-4aab-a2ea-52ac3767c6f4.1" data-mc-module-version="2019-10-22">
+      <tbody>
+        <tr>
+          <td style="padding:18px 30px 18px 30px; line-height:40px; text-align:inherit; background-color:#dde6de;" height="100%" valign="top" bgcolor="#dde6de" role="module-content"><div><div style="font-family: inherit; text-align: center"><span style="color: #6fab81; font-size: 40px; font-family: inherit">¡Hola ${name}, tu donación fue realizada con éxito!</span></div><div></div></div></td>
+        </tr>
+      </tbody>
+    </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="2f94ef24-a0d9-4e6f-be94-d2d1257946b0" data-mc-module-version="2019-10-22">
+      <tbody>
+        <tr>
+          <td style="padding:18px 50px 18px 50px; line-height:22px; text-align:inherit; background-color:#dde6de;" height="100%" valign="top" bgcolor="#dde6de" role="module-content"><div><div style="font-family: inherit; text-align: center"><span style="font-size: 16px; font-family: inherit">Estamos seguros que tus $ ${ammout} le serán de mucha ayuda a ${benefited}.</span></div>
+  <div style="font-family: inherit; text-align: center"><span style="font-size: 16px; font-family: inherit">Recuerda que si quieres apoyar aún más, puedes compartir la campaña con tus amigos y familiares.</span></div><div></div></div></td>
+        </tr>
+      </tbody>
+    </table><table border="0" cellpadding="0" cellspacing="0" align="center" width="100%" role="module" data-type="columns" style="padding:30px 0px 0px 0px;" bgcolor="#dde6de" data-distribution="1">
+      <tbody>
+        <tr role="module-content">
+          <td height="100%" valign="top"><table width="600" style="width:600px; border-spacing:0; border-collapse:collapse; margin:0px 0px 0px 0px;" cellpadding="0" cellspacing="0" align="left" border="0" bgcolor="" class="column column-0">
+        <tbody>
+          <tr>
+            <td style="padding:0px;margin:0px;border-spacing:0;"><table class="wrapper" role="module" data-type="image" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="ce6dd3be-5ed4-42d2-b304-55a58022cdf0">
+      <tbody>
+        <tr>
+          <td style="font-size:6px; line-height:10px; padding:0px 0px 0px 0px;" valign="top" align="center">
+            
+          <a href="https://images.pexels.com/photos/2072168/pexels-photo-2072168.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"><img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:100% !important; width:100%; height:auto !important;" width="600" alt="" data-proportionally-constrained="true" data-responsive="true" src="http://cdn.mcauto-images-production.sendgrid.net/4b279aeebde620d5/74c4f250-bdd6-47b7-9507-2b9bf3d6b052/1880x1253.jpeg"></a></td>
+        </tr>
+      </tbody>
+    </table></td>
+          </tr>
+        </tbody>
+      </table></td>
+        </tr>
+      </tbody>
+    </table><div data-role="module-unsubscribe" class="module" role="module" data-type="unsubscribe" style="color:#444444; font-size:12px; line-height:20px; padding:16px 16px 16px 16px; text-align:Center;" data-muid="4e838cf3-9892-4a6d-94d6-170e474d21e5"><div class="Unsubscribe--addressLine"><p class="Unsubscribe--senderName" style="font-size:12px; line-height:20px;">DonApp</p><p style="font-size:12px; line-height:20px;"><span class="Unsubscribe--senderAddress">Dirección</span>, <span class="Unsubscribe--senderCity">Perú+México</span>, <span class="Unsubscribe--senderState">♥</span> <span class="Unsubscribe--senderZip">123</span></p></div><p style="font-size:12px; line-height:20px;"><a class="Unsubscribe--unsubscribeLink" href="https://modest-darwin-5fa285.netlify.app/" target="_blank" style="">Unsubscribe</a> - <a href="https://modest-darwin-5fa285.netlify.app/" target="_blank" class="Unsubscribe--unsubscribePreferences" style="">Unsubscribe Preferences</a></p></div><table border="0" cellpadding="0" cellspacing="0" class="module" data-role="module-button" data-type="button" role="module" style="table-layout:fixed;" width="100%" data-muid="188c3d22-338c-4a35-a298-a7d3957f579d">
+        <tbody>
+          <tr>
+            <td align="center" bgcolor="" class="outer-td" style="padding:0px 0px 20px 0px;">
+              <table border="0" cellpadding="0" cellspacing="0" class="wrapper-mobile" style="text-align:center;">
+                <tbody>
+                  <tr>
+                  <td align="center" bgcolor="#f5f8fd" class="inner-td" style="border-radius:6px; font-size:16px; text-align:center; background-color:inherit;"><a href="https://modest-darwin-5fa285.netlify.app/" style="background-color:#f5f8fd; border:1px solid #f5f8fd; border-color:#f5f8fd; border-radius:25px; border-width:1px; color:#a8b9d5; display:inline-block; font-size:10px; font-weight:normal; letter-spacing:0px; line-height:normal; padding:5px 18px 5px 18px; text-align:center; text-decoration:none; border-style:solid; font-family:helvetica,sans-serif;" target="_blank">♥ Top-19 </a></td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table></td>
+                                        </tr>
+                                      </table>
+                                      <!--[if mso]>
+                                    </td>
+                                  </tr>
+                                </table>
+                              </center>
+                              <![endif]-->
+                            </td>
+                          </tr>
+                        </table>
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+            </table>
+          </div>
+        </center>
+      </body>
+    </html>`;
+};
 
 module.exports = {
   sendEmail,
   getTemplate,
+  donationSucTemplate,
 };
